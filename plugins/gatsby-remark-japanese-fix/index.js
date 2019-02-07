27f15ce9212bfed7989d7b109e1b627f34320e2e
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-02-08 01:40:31 +0900

    Fix typo, comment, function rename

--- plugins/gatsby-remark-japanese-fix/index.js
+++ plugins/gatsby-remark-japanese-fix/index.js
@@ -1,21 +1,22 @@
 const toString = require('mdast-util-to-string');
 const visit = require('unist-util-visit');
 
-const hasJapanese = str => {
-  // Detects katakana, hiragana, iteration mark (々), and CJK unified ideographs
+const hasJapaneseCharacter = str => {
+  // Detects katakana, hiragana, iteration marks, and CJK unified ideographs
   return /[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u4e00-\u9fea。、]/.test(str);
 };
 
 /**
- * Iterates over emphasises (<em>'s) within the AST created by Markdown docs.
- * Applies 'em-ja' class only when it contains some Japanese character,
- * so that it can be displayed differently.
+ * Iterates over emphases (<em>'s) within the AST created by remark.
+ * Applies 'em-ja' class only when it contains a Japanese character,
+ * so that it can be displayed with a different style.
  */
 
 module.exports = ({markdownAST}, options) => {
   visit(markdownAST, 'emphasis', node => {
     const nodeStr = toString(node);
-    if (hasJapanese(nodeStr)) {
+    if (hasJapaneseCharacter(nodeStr)) {
+      // Patch AST
       node.data = node.data || {};
       node.data.hProperties = node.data.hProperties || {};
       node.data.hProperties.class = 'em-ja';
