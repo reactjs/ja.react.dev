30abe94da34f6232f8f9aee8598c9c42d6037edd
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-02-09 03:35:55 +0900

    Add local textlint rules regarding spaces

--- textlint/jp-reactjs-org-lint.js
+++ textlint/jp-reactjs-org-lint.js
@@ -1,19 +1,82 @@
-/**
- * @param {RuleContext} context
- */
+const toString = require('mdast-util-to-string');
+
 module.exports = context => {
   return {
+    // [context.Syntax.Header]: node => {},
+
+    [context.Syntax.Paragraph]: node => {
+      // Checks against paragraph-based rules.
+
+      // Convert a paragraph to plain text, stripping any markups
+      const text = toString(node);
+
+      enforceZenHanSpace(node, text, context);
+      noHanPunctSpace(node, text, context);
+      enforceQuestionSpace(node, text, context);
+      noLineEndSpace(node, text, context);
+    },
+
     [context.Syntax.Str]: node => {
-      const text = context.getSource(node);
-      const match = /下さい/.exec(text);
-      if (match) {
+      const text = toString(node);
+      const index = text.indexOf('　');
+      if (index >= 0) {
         context.report(
           node,
           new context.RuleError(
-            `「下さい」の代わりに「ください」を使ってください。`,
+            'いかなる場合も全角スペースは使用しないでください。',
+            {index},
           ),
         );
       }
     },
   };
+};
+
+const enforceQuestionSpace = (node, text, context) => {
+  const markReg = /[！？](\u3000|[A-Za-z0-9]|[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])/;
+  if (markReg.exec(text)) {
+    context.report(
+      node,
+      new context.RuleError(
+        '全角の「！」「？」と次の文の間には半角スペースを挿入してください。',
+      ),
+    );
+  }
+};
+
+const noHanPunctSpace = (node, text, context) => {
+  if (/[、。]( |\u3000)/.exec(text)) {
+    context.report(
+      node,
+      new context.RuleError(
+        '全角の句読点の直後にスペースを入れてはいけません。',
+      ),
+    );
+  }
+};
+
+const enforceZenHanSpace = (node, text, context) => {
+  const hanZen = /[A-Za-z0-9](?:[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])/;
+  const zenHan = /(?:[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])[A-Za-z0-9]/;
+
+  if (hanZen.exec(text) || zenHan.exec(text)) {
+    context.report(
+      node,
+      new context.RuleError(
+        '全角文字と半角英数字とが隣接しています。半角スペースを挿入してください。',
+      ),
+    );
+  }
+};
+
+const noLineEndSpace = (node, text, context) => {
+  // This detects a line-end space *only when* the line contains Japanese characters.
+  const reg = /(?:[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ]).?(\s|\u3000)$/;
+
+  if (reg.exec(text)) {
+    context.report(
+      node,
+      new context.RuleError('行末にスペース文字を入れないでください。'),
+    );
+  }
 };
