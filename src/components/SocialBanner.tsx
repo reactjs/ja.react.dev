96308ff2a9a41bfc4a7b9b0cdb33957de615ae3f
Sakutaro Shina <208091987+NextReservation-Sakutaro@users.noreply.github.com>
2025-08-09 11:32:28 +0900

    React Confの参加依頼を英語から日本語に翻訳した。

--- src/components/SocialBanner.tsx
+++ src/components/SocialBanner.tsx
@@ -7,9 +7,9 @@ import {useRef, useEffect} from 'react';
 import cn from 'classnames';
 import {ExternalLink} from './ExternalLink';
 
-const bannerText = 'Join us for React Conf on Oct 7-8.';
+const bannerText = '10月7日-8日開催のReact Confに参加しませんか。';
 const bannerLink = 'https://conf.react.dev/';
-const bannerLinkText = 'Learn more.';
+const bannerLinkText = '詳細はこちら。';
 
 export default function SocialBanner() {
   const ref = useRef<HTMLDivElement | null>(null);
